944f3afdab652b37427f8356c0d4f6d56c7ab6fb
Renan Brayner <renan@archrenan.localdomain>
2020-09-27 12:09:13 -0300

    removed address and phone contact

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { Link, HashRouter as Router } from 'react-router-dom';
-import { FiHome, FiFeather, FiLayout, FiFileText, FiPhone, FiMail, FiGithub, FiLinkedin, FiFacebook  } from 'react-icons/fi';
+import {
+  FiHome,
+  FiFeather,
+  FiLayout,
+  FiFileText,
+  FiPhone,
+  FiMail,
+  FiGithub,
+  FiLinkedin,
+  FiFacebook,
+} from 'react-icons/fi';
 
 import DrawerToggleButton from '../DrawerToggleButton';
 import Backdrop from '../Backdrop';
@@ -11,54 +21,70 @@ import './styles.css';
 import imgUrl from '../../assets/profile.jpg';
 
 function NavBar() {
-    const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-    function toggleButtonHandleClick() {
-      setOpen( prevSetOpen => !prevSetOpen);
-    }
-  
-    function backdropHandleClick() {
-      setOpen(false);
-    }
-    
-    let backdrop;
-    
-    if(isOpen) {
-      backdrop = <Backdrop click={backdropHandleClick}/>;
-    }
+  function toggleButtonHandleClick() {
+    setOpen((prevSetOpen) => !prevSetOpen);
+  }
 
-    return(
-        <Router >
-            <SideDrawer show={isOpen} close={backdropHandleClick}/>
-            <div className='navbar-container'>
-                {backdrop}
-                <img className='logo' src={imgUrl} alt=''/>
-                <div className='branding'>
-                    <h1>Renan Brayner</h1>
-                    <a href='https://wa.me/5581981815475' target='_blank' rel='noopener noreferrer'><FiPhone className='icon' color='#3d55dd'/></a>
-                    <a href ='mailto: renanbrayner@gmail.com'><FiMail className='icon' color='#3d55dd'/></a>
-                    <a href='https://github.com/renanbrayner' target='_blank' rel='noopener noreferrer'><FiGithub className='icon' color='#3d55dd'/></a>
-                    <a href='https://www.linkedin.com/in/renan-brayner-a2a6b31a2/' target='_blank' rel='noopener noreferrer'><FiLinkedin className='icon' color='#3d55dd'/></a>
-                    <a href='https://www.facebook.com/renan.brayner.395' target='_blank' rel='noopener noreferrer'><FiFacebook className='icon' color='#3d55dd'/></a>
-                </div>
-                <DrawerToggleButton className='hamburguer' click={toggleButtonHandleClick}/>
-                <div className='navbar'>
-                    <Link className='link' to='/projetos'>
-                        <FiLayout color='#3d55dd' className='icon'/> Projetos
-                    </Link>
-                    <Link className='link' to='/sobre'>
-                        <FiFeather color='#3d55dd' className='icon'/> Sobre
-                    </Link>
-                    <Link className='link' to='/curriculo'>
-                        <FiFileText color='#3d55dd' className='icon'/> Curriculo
-                    </Link>
-                    <Link className='link' to='/'>
-                        <FiHome color='#3d55dd' className='icon'/> Home
-                    </Link>
-                </div>
-            </div>
-        </Router>
-    );
+  function backdropHandleClick() {
+    setOpen(false);
+  }
+
+  let backdrop;
+
+  if (isOpen) {
+    backdrop = <Backdrop click={backdropHandleClick} />;
+  }
+
+  return (
+    <Router>
+      <SideDrawer show={isOpen} close={backdropHandleClick} />
+      <div className="navbar-container">
+        {backdrop}
+        <img className="logo" src={imgUrl} alt="" />
+        <div className="branding">
+          <h1>Renan Brayner</h1>
+          <a href="mailto: renanbrayner@gmail.com">
+            <FiMail className="icon" color="#3d55dd" />
+          </a>
+          <a
+            href="https://github.com/renanbrayner"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiGithub className="icon" color="#3d55dd" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/renan-brayner-a2a6b31a2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiLinkedin className="icon" color="#3d55dd" />
+          </a>
+        </div>
+        <DrawerToggleButton
+          className="hamburguer"
+          click={toggleButtonHandleClick}
+        />
+        <div className="navbar">
+          <Link className="link" to="/projetos">
+            <FiLayout color="#3d55dd" className="icon" /> Projetos
+          </Link>
+          <Link className="link" to="/sobre">
+            <FiFeather color="#3d55dd" className="icon" /> Sobre
+          </Link>
+          <Link className="link" to="/curriculo">
+            <FiFileText color="#3d55dd" className="icon" /> Curriculo
+          </Link>
+          <Link className="link" to="/">
+            <FiHome color="#3d55dd" className="icon" /> Home
+          </Link>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default NavBar;
+
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,111 +1,125 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiGithub, FiLinkedin, FiFacebook, FiPhone, FiMail, } from 'react-icons/fi';
+import {
+  FiGithub,
+  FiLinkedin,
+  FiFacebook,
+  FiPhone,
+  FiMail,
+} from 'react-icons/fi';
 import Typist from 'react-typist';
 
-import './styles.css'
+import './styles.css';
 
 import SideDrawer from '../../components/SideDrawer';
-import Backdrop from '../../components/Backdrop'
+import Backdrop from '../../components/Backdrop';
 
 import pdfData from '../../utils/pdfData';
 
 import imgUrl from '../../assets/profile.jpg';
 
 function Home() {
-    const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-    function toggleButtonHandleClick() {
-      setOpen( prevSetOpen => !prevSetOpen);
-    }
-  
-    function backdropHandleClick() {
-      setOpen(false);
-    }
-    
-    let backdrop;
-    
-    if(isOpen) {
-      backdrop = <Backdrop click={backdropHandleClick}/>;
-    }
-    
-    return(
-        <div>
-        <SideDrawer show={isOpen} close={backdropHandleClick}/>
-        <div className='home-container'>
-            {backdrop}
-            <div className='content-container' >
-                <button className='mobile-navigation' onClick={toggleButtonHandleClick}>
-                    <div className='mobile-navigation-line'/>
-                    <div className='mobile-navigation-line'/>
-                    <div className='mobile-navigation-line'/>
-                </button>
-                <div className='branding'>
-                    <div className='branding-logo'>
-                        <img className='profile' src={imgUrl} alt=''/>
-                        <div className='branding-logo-text'>
-                            <h1 className='title'>Renan Brayner</h1>
-                            <Typist className='text' cursor={{element: '.'}}>
-                                <span>Desenvolvedor Web</span>
-                                <Typist.Backspace count={3} delay={1200} />
-                                <span>Mobile</span>
-                                <Typist.Backspace count={6} delay={1200} />
-                                <span>React JS</span>
-                                <Typist.Backspace count={8} delay={1200} />
-                                <span>React Native</span>
-                                <Typist.Backspace count={12} delay={1200} />
-                                <span>Node JS</span>
-                                <Typist.Backspace count={8} delay={1200} />
-                            </Typist>
-                        </div>
-                    </div>
-                    <div className='contact-info'>
-                        <ul className='contact-info-list'>
-                            <li>Rua João Alves Mendes, 470 – Apt. 302</li>
-                            <li>renanbrayner@gmail.com</li>
-                            <li>+55 (81) 98181-5475</li>
-                            <ul className='contact-info-icons'>
-                                <li>
-                                    <a href='https://github.com/renanbrayner' target='_blank' rel='noopener noreferrer'>
-                                        <FiGithub size={20} color='#3d55dd' className='icon' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='https://www.linkedin.com/in/renan-brayner-a2a6b31a2/' target='_blank' rel='noopener noreferrer'>
-                                        <FiLinkedin size={20} color='#3d55dd' className='icon' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='https://www.facebook.com/renan.brayner.395' target='_blank' rel='noopener noreferrer'>
-                                        <FiFacebook size={20} color='#3d55dd' className='icon' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='https://wa.me/5581981815475' target='_blank' rel='noopener noreferrer'>
-                                        <FiPhone size={20} color='#3d55dd' className='icon' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='mailto: renanbrayner@gmail.com'>
-                                        <FiMail size={20} color='#3d55dd' className='icon' />
-                                    </a>
-                                </li>
-                            </ul>
-                        </ul>
-                    </div>
-                </div>
-                <nav className='navigation'>
-                    <div className='links'>
-                        <Link className='link primeiro' to='/projetos'>Projetos</Link>
-                        <Link className='link' to='/sobre'>Sobre</Link>
-                        <Link className='link ultimo' to='/curriculo'>Curriculo</Link>
-                        <a className='link mobile' href={pdfData} download>Curriculo</a>
-                    </div>
-                </nav>
+  function toggleButtonHandleClick() {
+    setOpen((prevSetOpen) => !prevSetOpen);
+  }
+
+  function backdropHandleClick() {
+    setOpen(false);
+  }
+
+  let backdrop;
+
+  if (isOpen) {
+    backdrop = <Backdrop click={backdropHandleClick} />;
+  }
+
+  return (
+    <div>
+      <SideDrawer show={isOpen} close={backdropHandleClick} />
+      <div className="home-container">
+        {backdrop}
+        <div className="content-container">
+          <button
+            className="mobile-navigation"
+            onClick={toggleButtonHandleClick}
+          >
+            <div className="mobile-navigation-line" />
+            <div className="mobile-navigation-line" />
+            <div className="mobile-navigation-line" />
+          </button>
+          <div className="branding">
+            <div className="branding-logo">
+              <img className="profile" src={imgUrl} alt="" />
+              <div className="branding-logo-text">
+                <h1 className="title">Renan Brayner</h1>
+                <Typist className="text" cursor={{ element: '.' }}>
+                  <span>Desenvolvedor Web</span>
+                  <Typist.Backspace count={3} delay={1200} />
+                  <span>Mobile</span>
+                  <Typist.Backspace count={6} delay={1200} />
+                  <span>React JS</span>
+                  <Typist.Backspace count={8} delay={1200} />
+                  <span>React Native</span>
+                  <Typist.Backspace count={12} delay={1200} />
+                  <span>Node JS</span>
+                  <Typist.Backspace count={8} delay={1200} />
+                </Typist>
+              </div>
             </div>
+            <div className="contact-info">
+              <ul className="contact-info-list">
+                <li>renanbrayner@gmail.com</li>
+                <ul className="contact-info-icons">
+                  <li>
+                    <a
+                      href="https://github.com/renanbrayner"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FiGithub size={20} color="#3d55dd" className="icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/renan-brayner-a2a6b31a2/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FiLinkedin size={20} color="#3d55dd" className="icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="mailto: renanbrayner@gmail.com">
+                      <FiMail size={20} color="#3d55dd" className="icon" />
+                    </a>
+                  </li>
+                </ul>
+              </ul>
+            </div>
+          </div>
+          <nav className="navigation">
+            <div className="links">
+              <Link className="link primeiro" to="/projetos">
+                Projetos
+              </Link>
+              <Link className="link" to="/sobre">
+                Sobre
+              </Link>
+              <Link className="link ultimo" to="/curriculo">
+                Curriculo
+              </Link>
+              <a className="link mobile" href={pdfData} download>
+                Curriculo
+              </a>
+            </div>
+          </nav>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
+
